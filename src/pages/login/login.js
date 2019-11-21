e8ad88c49eb31ad9fabf13c7a4cc5526da28cbcd
Robin <544768940@qq.com>
2019-11-22 06:30:25 +0800

    feature: page login

---
+++ src/pages/login/login.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import {
+    Form,
+    Icon,
+    Input,
+    Button,
+    message
+} from 'antd'
+
+import logo from '../../assets/images/arr.png'
+
+import './login.less'
+
+const Item = Form.Item
+
+class Login extends Component{
+    
+    handleSubmit = (event)=>{
+        event.preventDefault()
+        this.props.form.validateFields((err, values)=>{
+            if(!err){
+                console.log(values)
+            }
+        })
+    }
+    validatorPwd = (rule, value, callback)=>{
+        if(!value) {
+            callback('密码必须输入')
+        } else if (value.length<4) {
+            callback('密码长度不能小于4位')
+        } else if (value.length>12) {
+            callback('密码长度不能大于12位')
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            callback('密码必须是英文、数字或下划线组成')
+        } else {
+            callback() // 验证通过
+        }
+    }
+
+    render(){
+        const form = this.props.form
+        const { getFieldDecorator } = form
+
+        return(
+            <div className='login'>
+                <header className='login-header'>
+                    <img src={logo} alt=""/>
+                    <h1>后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <Form onSubmit={this.handleSubmit}  className='login-form'>
+                        <Item>
+                            {
+                                getFieldDecorator('username', {
+                                    rules: [
+                                        { required: true, whitespace: true, message: '用户名必须输入' },
+                                        { min: 4, message: '用户名至少4位' },
+                                        { max: 12, message: '用户名最多12位' },
+                                        { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成' }, 
+                                    ],
+                                    initialValue: 'admin'
+                                })(
+                                    <Input
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        placeholder="用户名"
+                                    />
+                                )
+                            }
+                        </Item>
+                        <Item>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        {
+                                            validator: this.validatorPwd
+                                        } 
+                                    ],
+                                    initialValue: '123456'
+                                })(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type='password'
+                                        placeholder="密码"
+                                    />
+                                )
+                            }
+                        </Item>
+                        <Form.Item>
+                            <Button type='primary' htmlType='submit' className='login-form-button'>提交</Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Form.create()(Login)
