30d51cc42e6b6e1d387baca0f08a3da22a690cc5
Robin <544768940@qq.com>
2019-11-23 01:11:20 +0800

    feat: header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,91 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Modal } from 'antd'
+
+import LinkButton from '../link-button'
+import { reqWeather } from '../../api'
+import menuList from '../../config/menuConfig'
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import storageUtils from '../../utils/storageUtils'
 
 import './index.less'
-export default class Header extends Component{
+class Header extends Component{
+
+    state = {
+        currentTime: formateDate(Date.now()),
+        dayPictureUrl: '', // 天气图片url
+        weather: '', // 天气的文本
+    }
+
+    getTime = ()=>{
+        this.intervalId = setInterval(()=>{
+            const currentTime = formateDate(Date.now())
+            this.setState({ currentTime })
+        }, 1000)
+    }
+    getWeather = async()=>{
+        const { dayPictureUrl, weather } = await reqWeather('北京')
+        console.log(dayPictureUrl, weather )
+        this.setState({ dayPictureUrl, weather })
+    }
+    //面包屑
+    getTitle = ()=>{
+        const path = this.props.location.pathname
+        let title
+        menuList.forEach(item=>{
+            if(item.key === path){
+                title = item.title
+            }else if(item.children){
+                const cItem = item.children.find(cItem => path.indexOf(cItem.key) === 0)
+                if(cItem){
+                    title = cItem.title
+                }
+            }
+        })
+        return title
+    }
+
+    logout = ()=>{
+        Modal.confirm({
+            content: '确定退出吗?',
+            onOk: ()=>{
+                console.log('OK', this)
+                storageUtils.removeUser()
+                memoryUtils.user = {}
+                this.props.history.replace('/login')
+            }
+        })
+    }
+
+    componentDidMount(){
+        this.getTime()
+        this.getWeather()
+    }
+    componentWillUnmount(){
+        clearInterval(this.intervalId)
+    }
     render(){
+        const { currentTime, dayPictureUrl, weather } = this.state
+        const username = memoryUtils.user.username
+        const title = this.getTitle()
         return(
             <div className='header'>
-                header
+                <div className="header-top">
+                    <span>欢迎,{username}</span>
+                    <LinkButton onClick={this.logout}>exit</LinkButton>
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">{title}</div>
+                    <div className="header-bottom-right">
+                        <span>{currentTime}</span>
+                        <img src={dayPictureUrl} alt="weather"/>
+                        <span>{weather}</span>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withRouter(Header)
